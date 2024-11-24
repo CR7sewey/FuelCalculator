fc1b85b1b06e430f7cfb646dc88b4f0f521b6d7d
CR7sewey <miguelmotacarvalho@gmail.com>
2024-11-24 19:35:12 +0000

    Distance (DistanceActivity)

--- app/src/main/java/com/example/fuelcalculator/DistanceActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/DistanceActivity.kt
@@ -10,36 +10,35 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
-const val PRICE_FUEL = "price_fuel"
-const val PRICE_CAR = "price_car"
-
-
-class CarConsActivity : AppCompatActivity() {
+const val DISTANCE = "distance"
+class DistanceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_car_cons)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.car)) { v, insets ->
+        setContentView(R.layout.activity_distance)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.distance)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val price = findViewById<TextInputEditText>(R.id.priceCar)
-        val next = findViewById<Button>(R.id.next3)
+        val distance = findViewById<TextInputEditText>(R.id.distanceCar)
+        val next = findViewById<Button>(R.id.next4)
         val priceFuel: String? = intent.getStringExtra(PRICE_FUEL)
+        val priceCar: String? = intent.getStringExtra(PRICE_CAR)
 
         next.setOnClickListener {
-            val priceStr = price.text.toString()
-            if (priceStr.isEmpty()) {
+            val distanceStr = distance.text.toString()
+            if (distanceStr.isEmpty()) {
                 Snackbar.make(next, "Please, insert a value!", Snackbar.LENGTH_LONG).show()
             }
             else {
                 val explicitIntent = Intent(this, DistanceActivity::class.java)
                 explicitIntent.putExtra(PRICE_FUEL,priceFuel)
-                explicitIntent.putExtra(PRICE_CAR, priceStr)
+                explicitIntent.putExtra(PRICE_CAR, priceCar)
+                explicitIntent.putExtra(DISTANCE, distanceStr)
                 startActivity(explicitIntent)
             }
         }
+
     }
 }
