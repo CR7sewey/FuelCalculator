5fb2f8cf9d7e991aa5e62be69dd84048b6b04c6d
CR7sewey <miguelmotacarvalho@gmail.com>
2024-11-25 18:37:32 +0000

    Result - Part 2

--- app/src/main/java/com/example/fuelcalculator/ResultActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/ResultActivity.kt
@@ -1,10 +1,15 @@
 package com.example.fuelcalculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,35 @@ class ResultActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val price: String? = intent.getStringExtra(PRICE_FUEL)
+        val consumption: String? = intent.getStringExtra(PRICE_CAR)
+        val distance: String? = intent.getStringExtra(DISTANCE)
+
+        var explicitIntent: Intent
+
+        if (price.isNullOrBlank() || consumption.isNullOrBlank() || distance.isNullOrBlank()) {
+            explicitIntent = Intent(this, PriceActivity::class.java)
+            startActivity(explicitIntent)
+        }
+
+        val result = findViewById<TextView>(R.id.result)
+        val resultBD = BigDecimal(price?.toDouble()!! * consumption?.toDouble()!!/100 * distance?.toDouble()!!).setScale(1, RoundingMode.HALF_EVEN)
+        result.text = resultBD.toString() + "€"
+
+        val priceLabel = findViewById<TextView>(R.id.priceFuel)
+        priceLabel.text = price
+        val consumptionLabel = findViewById<TextView>(R.id.consumption)
+        consumptionLabel.text = consumption
+        val distanceLabel = findViewById<TextView>(R.id.distance)
+        distanceLabel.text = distance
+
+        val newCalc = findViewById<Button>(R.id.next5)
+
+        newCalc.setOnClickListener {
+            explicitIntent = Intent(this, MainActivity::class.java)
+            startActivity(explicitIntent)
+        }
+
     }
 }
